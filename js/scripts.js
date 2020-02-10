c639dade227c2ae0aa1e33f749e25b1c9ec83356
Brito <britoarteiro@gmail.com>
2020-02-10 18:13:24 -0300

    ul li

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,14 @@
 $.ajax({
     url: "https://api.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=3f4da57b890f31f1ef9b7fe64fb02e7d&gallery_id=72157713056458181&user_id=186906858@N06&format=json&nojsoncallback=?", 
     success: function(result){
-  console.log(result.photos.photo);
   var imgs = result.photos.photo;
-  console.log(imgs.length);
   
         for(i=0;i<imgs.length;i++){
             var img = imgs[i];
             console.log(img);
+            var li = "<li></li>";
+            li.css("background-image","url(https://live.staticflickr.com/"+ img.server +"/"+ img.id +"_"+ img.secret +"_z.jpg)");
+            $('#imgList').append(li);
         }
-
-//   $(imgs).each(function(){
-//     var li = "<li>" + imgs + "</li>"
-//     $('#imgList').append(li);
-//   })
 
 }});
